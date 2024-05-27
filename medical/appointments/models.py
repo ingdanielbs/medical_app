dba8266da1ff11021be2c47812a9ab18db993388
ingdanielb <danielb2889@misena.edu.co>
2024-05-27 06:06:47 -0500

    update

--- medical/appointments/models.py
+++ medical/appointments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Appointment(models.Model):    
+    type_appointment = models.CharField(max_length=50)
+    date = models.DateField()
+    time = models.TimeField()
+    doctor = models.ForeignKey('doctors.Doctor', on_delete=models.DO_NOTHING)
+    patient = models.ForeignKey('patients.Patient', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.type_appointment
--- medical/doctors/models.py
+++ medical/doctors/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    document = models.CharField(max_length=50, unique=True)
+    address = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=50)
+    status = models.BooleanField(default=True)
+    specialty = models.ForeignKey('specialties.Specialty', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
